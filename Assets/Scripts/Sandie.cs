f044b41ca891e48f9325b7ec42f07cd1abe3f8ea
Lucas Cohen <lucohen@chapman.edu>
2025-01-31 20:28:11 -0800

    Added character scripts

---
+++ Assets/Scripts/Sandie.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sandie : Character
+{
+    // Start is called before the first frame update
+    public int laserCooldown;
+    public int leechCooldown;
+    public int heavyThrowCooldown;
+}
