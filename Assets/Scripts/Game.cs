3b0e105d9d52d3511293e316150d78203f038ab6
Lucas Cohen <lucohen@chapman.edu>
2025-02-01 15:47:05 -0800

    WIP on main

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    InputFieldHandler inputField;
-    private Player player1;
+    [SerializeField] public InputFieldHandler inputField;
+    [SerializeField] private Player player1;
     private Player player2;
 
     private MoveData player1MoveData;
